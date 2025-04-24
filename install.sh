8643b7ea8b3345102a4583cbf2164c62d159b995
ysyyayyyayyay <landonzyin@gmail.com>
2025-04-24 10:49:49 -0400

    fix installer

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cd SigVM
 pip install textual
 sleep 2
 python3 installer.py
-docker build -t blobevm . --no-cache
+docker build -t sigvm . --no-cache
 cd ..
 
 sudo apt update
@@ -14,9 +14,9 @@ cp -r SigVM/root/config/* Save
 
 json_file="SigVM/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
-    docker run -d --name=BlobeVM -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=BlobeVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped blobevm
+    docker run -d --name=SigVM -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=SigVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped sigvm
 else
-    docker run -d --name=BlobeVM -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=BlobeVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped blobevm
+    docker run -d --name=SigVM -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=SigVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped sigvm
 fi
 clear
 echo "SIGVM WAS INSTALLED SUCCESSFULLY! Check Port Tab"
---
+++ root/installable-apps/mods.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install default-jre
+sudo apt --fix-broken install
+curl -o https://raw.github.com/ysyyayyyayya/sigclientmods
